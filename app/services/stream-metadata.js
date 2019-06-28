fa08c6ff22185b05fcb8638d6f862c39f6a7b0bb
Bryn Scharenberg <bscharenberg@nypublicradio.org>
2019-06-28 11:04:23 -0400

    copy playlist service from new-sounds and modify to be able to get the playlist of multiple streams.

---
+++ app/services/stream-metadata.js
@@ -0,0 +1,218 @@
+//adapted from new-sounds-web-client/playlist.js
+import Service from '@ember/service';
+import config from '../config/environment';
+import { inject as service} from '@ember/service';
+import { computed, observer, get, set } from '@ember/object';
+import fetch from 'fetch';
+import moment from 'moment';
+import { debug } from '@ember/debug';
+import fixTimestamp from '../helpers/fix-timestamp';
+
+
+var DEFAULTSHOWDICT = {'wqxr': 'terrance-mcknight'}
+var DEFAULTSHOWTITLEDICT = {'wqxr': 'Evenings with Terrance McKnight'}
+
+
+export default Service.extend({
+  host: config.publisherAPI,
+  store: service(),
+  hifi: service(),
+  poll: service(),
+  currentStream : service(),
+  isLoading: false,
+  dailySchedule: null,
+  dailyScheduleUpdater: observer('currentStream.slug', function() {
+    if (!this.get('isLoading')) {
+      this.load(moment().format());
+    }
+  }),
+  scheduleDate: moment().format("YYYY/MMM/DD"), //currentScheduleId
+  isTodaysSchedule: computed('scheduleDate', function() {
+    return moment().format('YYYY/MMM/DD') == get(this, 'scheduleDate');
+  }),
+  previousScheduleDate: computed('scheduleDate', function() {
+    return moment(get(this, 'scheduleDate'), 'YYYY/MMM/DD').subtract(1, 'day').format('YYYY/MMM/DD')
+  }),
+  nextScheduleDate: computed('scheduleDate', function() {
+    return moment(get(this, 'scheduleDate'), 'YYYY/MMM/DD').add(1, 'day').format('YYYY/MMM/DD')
+  }),
+
+  init() {
+    let pollFunction = () => this.refreshStream();
+    let pollId = this.get('poll').addPoll({interval: 10 * 1000, callback: pollFunction});
+    this.set('pollId', pollId);
+
+    this._super(...arguments);
+  },
+
+  loadStreams() {
+    return this.store.findAll('stream', {reload: true}).then(s => {
+      s = s.filterBy('liveWQXR').filterBy('hasPlaylists').sortBy('sitePriority').uniq();
+      this.set('streams', s)
+      return s;
+    })
+  },
+
+  load(date) {
+    this.set('isLoading', true)
+    this.set('scheduleDate', moment(date).format('YYYY/MMM/DD'));
+    let prefetchedSchedule = get(this, 'store').peekRecord('daily-schedule', `${moment(date).format('YYYY/MMM/DD')}-${this.get('currentStream.slug')}`)
+    if (prefetchedSchedule) {
+      this.set('isLoading', false)
+      this.set('dailySchedule', prefetchedSchedule);
+    } else {
+      let requestUrl = this._scheduleUrlForDate(date);
+      return this.getSchedule(requestUrl);
+    }
+  },
+
+  refreshStream() {
+    let promise = get(this, 'store').findRecord('stream', this.get('currentStream.slug'))
+    promise.then(response => this.processWhatsOn(response));
+    return promise;
+  },
+
+  processWhatsOn(response) {
+    set(this, 'nowPlayingId', "entry_" + get(response, 'currentPlaylistItem.playlistEntryId'))
+
+    let newTrack = get(response, 'currentPlaylistItem');
+    if (newTrack) {
+      let store     = get(this, 'store')
+      let startTime = get(newTrack, 'startTime')
+      let trackId   = "entry_" + get(newTrack, 'playlistEntryId');
+      if (trackId && !store.peekRecord('track', trackId)) {
+        newTrack = store.createRecord('track', {
+          id             : trackId,
+          playlistEntryId: trackId,
+          catalogEntry   : get(newTrack, 'catalogEntry'),
+          startTime      : moment(startTime).format('hh:mm a'),
+        });
+      } else {
+        newTrack = null;
+      }
+    }
+
+    let newShow = get(response, 'currentShow')
+    if (newShow) {
+      let airing = this.findAiring(fixTimestamp(get(newShow, 'start')));
+      if (airing && newTrack) {
+        let tracks = [newTrack];
+        tracks.pushObjects(get(airing, 'tracks'))
+        set(airing, 'tracks', tracks)
+      }
+      if(airing && get(newShow, 'episodePk')) {
+        this.get('store').findRecord('story', get(newShow, 'episodePk')).then(story => {
+          airing.set('episode', story);
+        });
+      }
+      if(airing && !get(airing, 'isCurrent')){
+        set(airing, 'isCurrent', true)
+      }
+      // if airing not found, do nothing with track. most likely scenario is that
+      // the clock hit midnight and the next day's schedule has not been loaded.
+    }
+  },
+
+  getSchedule(requestUrl) {
+    debug(`getting schedule ${requestUrl}`);
+    return fetch(requestUrl).then(response => {
+      return response.json().then(r => {
+        debug(`process schedule ${requestUrl}`)
+        return this.processSchedule(r);
+      })
+    });
+  },
+
+  processSchedule(results) {
+    var dailySchedule = get(this, 'store').createRecord('daily-schedule', {airings: [], id: `${this.get('scheduleDate')}-${this.get('currentStream.slug')}`})
+    //return
+    results.events.map(event => {
+      var attrs;
+      var playlists = event.playlists.map(p => p.played.map(t => this.processTrack(t)))
+      if (get(event, 'isObject')) {
+        var matches = event.show_url.match('shows/(.+)');
+        let showSlug = matches ? matches[1] : DEFAULTSHOWDICT[this.get('currentStream.slug')]
+
+        attrs = {
+          id       : `${event.start_timestamp}_${showSlug}`,
+          startTime: event.start_timestamp,
+          endTime  : event.end_timestamp,
+          title    : event.show_title,
+          showId   : event.show_id,
+          showSlug : showSlug,
+          tracks : [].concat.apply([], playlists).sort(function(a, b) {
+                      return (moment(get(a, 'startTime'), 'hh:mm a') > moment(get(b, 'startTime'), 'hh:mm a') ? -1 : 1);
+                   })
+        };
+      }
+      else {
+        attrs = {
+          id       : `${event.start_timestamp}_${DEFAULTSHOWDICT[this.get('currentStream.slug')]}`,
+          startTime: event.start_timestamp,
+          endTime  : fixTimestamp(event.end_timestamp),
+          title    : DEFAULTSHOWTITLEDICT[this.get('currentStream.slug')],
+          showSlug : DEFAULTSHOWDICT[this.get('currentStream.slug')],
+          tracks : [].concat.apply([], playlists).sort(function(a, b) {
+                      return (moment(get(a, 'startTime')) > moment(get(b, 'startTime'), 'hh:mm a') ? -1 : 1);
+                   })
+        };
+      }
+
+      // TODO: change this model from airing to something less confusing. This is
+      // basically a event - and it can either be a show event or airing, as defined in publisher.
+      var airing = get(this, 'store').createRecord('airing', attrs)
+
+      get(this, 'store').findRecord('show', attrs.showSlug).then(show => {
+        airing.set('show', show);
+      })
+
+      set(this, 'dailySchedule', dailySchedule);
+      dailySchedule.get('airings').addObject(airing);
+    });
+    this.set('isLoading', false);
+    return dailySchedule;
+  },
+
+  _scheduleUrlForDate(rawDate) {
+    let date  = moment.tz(rawDate, "America/New_York");
+    let slug  = this.get('currentStream.slug');
+    let year  = date.format("YYYY");
+    let month = date.format('MMM').toLowerCase();
+    let day   = date.format('DD');
+
+    let url = `${this.host}/v1/playlist-daily/${slug}/${year}/${month}/${day}/`;
+    return url;
+  },
+
+  processTrack(track) {
+    let store     = get(this, 'store')
+    let trackId   = get(track, 'id');
+    if (trackId && !store.peekRecord('track', trackId)) {
+      let record = store.createRecord('track', {
+        id             : get(track, 'id'),
+        playlistEntryId: get(track, 'id'),
+        catalogEntry   : get(track, 'info'),
+        startTime      : get(track, 'time'),
+      });
+      return record;
+    } else if (trackId) {
+      return store.peekRecord('track', trackId);
+    } else {
+      return null;
+    }
+  },
+
+  findAiring(timestampWithZone) {
+    let playTime = moment(timestampWithZone);
+
+    let airing = get(this, 'store').peekAll('airing').find(s => {
+      return playTime.isBetween(moment(get(s, 'startTime')), moment(get(s, 'endTime')), null, '[]');
+    });
+
+    if (!airing) {
+      debug(`could not find schedule item for track ${timestampWithZone} -> ${playTime}`)
+    }
+
+    return airing;
+  }
+});
