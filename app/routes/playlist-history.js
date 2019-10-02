aa79b4eac10438562e4c3cf4e9d8540f9a48b864
Brad Kammin <bkammin@nypublicradio.org>
2019-10-02 11:55:50 -0400

    DSODA-94 lint cleanup

--- app/routes/playlist-history.js
+++ app/routes/playlist-history.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model(params, transition) {
+  model() {
     let controller = this.controllerFor('application');
     controller.send('setNavSlug', 'playlist-history');
   },
