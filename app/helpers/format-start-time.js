155c45a78b609639f5589e228e943ab89ac6edb0
Brad Kammin <bkammin@nypublicradio.org>
2019-10-04 13:31:32 -0400

    DSODA-111 cleanup

--- app/helpers/format-start-time.js
+++ app/helpers/format-start-time.js
@@ -1,4 +1,5 @@
 import { helper } from '@ember/component/helper';
+import moment from 'moment';
   
 export function formatStartTime(timestamp) {
   return moment(timestamp,'YYYY-MM-DDThh:mm:ss').format('h:mm A');
