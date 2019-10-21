8ad3d81b65b68302aac1226b1353788689aeb05b
Jeff Keen <jeff@keen.me>
2019-10-21 18:59:45 -0500

    Upgrade to ember 3.9, rev dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,28 @@ module.exports = {
     browser: true
   },
   rules: {
-  }
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        '.eslintrc.js',
+        '.template-lintrc.js',
+        'ember-cli-build.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'lib/*/index.js',
+        'server/**/*.js'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      }
+    }
+  ]
 };
