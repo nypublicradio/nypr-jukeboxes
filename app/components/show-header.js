547cba1aadc79ee5fa3044eb218081f1d1dfea39
Seori <seori@gmail.com>
2019-10-22 19:23:43 -0400

    Sticky header logic in the sticky-element ember dependency

--- app/components/show-header.js
+++ app/components/show-header.js
@@ -1,31 +1,12 @@
 import Component from '@ember/component';
 import { get, set } from '@ember/object';
 
-function isAboveViewport(element) {
-  var rect = element.getBoundingClientRect();
-  return rect.top > 0;
-}
-
-let triggerStickyClass = function () {
-  let sentinel = document.querySelector(".new-donate");
-
-  if (!isAboveViewport(sentinel)) {
-    let denim = [0, 81, 142];
-    document.getElementById('sticky-donate').style.visibility = 'visible';
-    document.getElementById('listen-playhistory').style.backgroundColor = 'rgb(' + [...denim].join(',') + ')';
-  } else {
-    document.getElementById('sticky-donate').style.visibility = 'hidden';
-    document.getElementById('listen-playhistory').style.backgroundColor = 'transparent';
-  }
-};
-
 export default Component.extend({
   classNames: ['show-header'],
   didRender() {
     this._super(...arguments);
     window.addEventListener(
-      "scroll",
-      triggerStickyClass
+      "scroll"
     );
   },
   activeTabIndex: null,
@@ -36,5 +17,8 @@ export default Component.extend({
         activeTabIndex: get(this, 'activeTabIndex')
       })
     }
+  },
+  willDestroyElement(){
+    this._super(...arguments);
   }
 });
