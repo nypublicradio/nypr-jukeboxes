3dda2dc6f2a7ef843c25847e5af0db1ef33a1bb6
Brad Kammin <bkammin@nypublicradio.org>
2019-10-24 07:49:06 -0400

    DSODA-113 lint cleanup

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,4 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
-import RSVP from 'rsvp';
-import moment from 'moment';
 
 export default Route.extend({
 
