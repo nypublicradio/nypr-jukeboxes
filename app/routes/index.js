e7e5c2273dabfdc8b4675026ba75f0046227380a
Brad Kammin <bkammin@nypublicradio.org>
2019-10-24 07:43:45 -0400

    DSODA-113 clean up index.js/.hbs

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,16 +3,6 @@ import { inject as service } from '@ember/service';
 import RSVP from 'rsvp';
 import moment from 'moment';
 
-
 export default Route.extend({
-  fastboot: service(),
-  streamMetadata: service(),
 
-  model() {
-    let hash = {
-      //TODO: move this to the play-schedule component so it doesn't block render
-      playlist: this.get('streamMetadata').load(moment().format())
-    };
-    return RSVP.hash(hash);
-  },
 });
