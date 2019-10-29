6bfc96ad5813ebd4a2adc45007b802539932d083
Brad Kammin <bkammin@nypublicradio.org>
2019-10-29 16:40:18 -0400

    DSODA-159 style, VQA tweaks

--- app/helpers/format-start-time.js
+++ app/helpers/format-start-time.js
@@ -2,7 +2,16 @@ import { helper } from '@ember/component/helper';
 import moment from 'moment';
   
 export function formatStartTime(timestamp) {
-  return moment(timestamp,'YYYY-MM-DDTHH:mm:ss').format('h:mm A');
+  let momentStartTime = moment(timestamp,'YYYY-MM-DDTHH:mm:ss');
+  if (momentStartTime.isValid()) {
+    return momentStartTime.format('h:mm A');
+  } else {
+    momentStartTime = moment(timestamp,'hh:mm A');
+    if (momentStartTime.isValid()) {
+      return momentStartTime.format('h:mm A');
+    }
+  }
+  return timestamp;
 }
 
 export default helper(formatStartTime);
