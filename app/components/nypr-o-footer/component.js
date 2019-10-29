dfd101d6c5b8060392d92f8b1aa6f4ac3d3b112a
Seori <seori@gmail.com>
2019-10-29 16:26:13 -0400

    Putting horizontal hbs back

--- app/components/nypr-o-footer/component.js
+++ app/components/nypr-o-footer/component.js
@@ -1,11 +1,11 @@
 import Component from '@ember/component';
 import layout from './template';
-import { positionBottomCenter } from '@nypr-design-system' ;
+// import { positionBottomCenter } from '@nypr-design-system' ;
 
 export default Component.extend({
   layout,
   tagName: '',
-  calculatePosition(trigger, content) {
-    return positionBottomCenter(trigger, content, 5);
-  }
+  // calculatePosition(trigger, content) {
+  //   return positionBottomCenter(trigger, content, 5);
+  // }
 });
