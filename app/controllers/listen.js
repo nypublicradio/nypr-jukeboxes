45832b29118953a452625e53affbc4b2416135ff
Brad Kammin <bkammin@nypublicradio.org>
2019-10-30 11:54:30 -0400

    DSODA-159 pull current track fields from computed/read properties

--- app/controllers/listen.js
+++ app/controllers/listen.js
@@ -1,8 +1,10 @@
 import Controller from '@ember/controller';
 import { inject as controller } from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
   appController: controller('application'),
+  currentStream  : service(),
 
   actions: {
     updatePlayerState(state) {
