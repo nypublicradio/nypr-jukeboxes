c4d05dbd1285bc21719a5988691c340adec2ff47
Brad Kammin <bkammin@nypublicradio.org>
2019-11-05 15:28:24 -0500

    DSODA-219 If Play History Preview was hidden, and now What's On is populating, should show Play History Preview as follows: 1. If one song has completed since Play History Preview was hidden, show just that one song in the Preview. 2. If two songs have completed since Play History Preview was hidden, show just those two songs in the Preview. 3. If three songs have completed since Play History Preview was hidden, show all three songs. 4. Play History Preview shows up to three of the most recent songs. 5. Design: https://app.zeplin.io/project/5d83c520c2bfab889196fdf1/dashboard?seid=5db9eac876af0057d9843902

--- app/controllers/listen.js
+++ app/controllers/listen.js
@@ -7,21 +7,42 @@ import moment from 'moment';
 export default Controller.extend({
   appController: controller('application'),
   currentStream  : service(),
-  SHOW_STALE_CUTOFF: 15 * 60 * 1000,
+  SHOW_STALE_CUTOFF: 15 * 60,
 
   isPlaylistHistoryPreviewStale: Ember.computed('clock.minute', function () {
-    let nowMs = moment().valueOf();
-    let showStartTimeMs = this.model.stream.currentShow.start_ts * 1000;
-    let trackStartTimeMs = 0;
+    let nowTs = moment().valueOf() / 1000;
+    let showStartTimeTs = this.model.stream.currentShow.start_ts;
+    let trackStartTimeTs = 0;
 
     if (this.model.stream.previous && this.model.stream.previous.length > 0) {
-      trackStartTimeMs =  this.model.stream.previous[0].startTimeTs * 1000;
+      trackStartTimeTs =  this.model.stream.previous[0].startTimeTs;
     }
 
-    if (nowMs > (showStartTimeMs + this.SHOW_STALE_CUTOFF) && (trackStartTimeMs < showStartTimeMs)) {
+    if (nowTs > (showStartTimeTs + this.SHOW_STALE_CUTOFF) && (trackStartTimeTs < showStartTimeTs)) {
       return true;
     }
     return false;
+  }),
+
+  playlistHistoryItems: Ember.computed('model.stream.previous', function() {
+    let firstTrackFromCurrentShowStartTimeTs = 0;
+    let showStartTimeTs = this.model.stream.currentShow.start_ts;
+    let currentShowTracks = [];
+
+    if (this.model.stream.previous) {
+      currentShowTracks = this.model.stream.previous.filter( (track) => {
+        return track.startTimeTs >= showStartTimeTs;
+      });
+      if (currentShowTracks.length > 0) {
+        firstTrackFromCurrentShowStartTimeTs = currentShowTracks[currentShowTracks.length - 1].startTimeTs;
+      }
+    }
+
+    if (firstTrackFromCurrentShowStartTimeTs > (showStartTimeTs + this.SHOW_STALE_CUTOFF)) {
+      return currentShowTracks;
+    }
+
+    return this.model.stream.previous;
   }),
 
   actions: {
