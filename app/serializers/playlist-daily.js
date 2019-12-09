144e5d4637381405a8fc576002e879dd80c3b1b4
Brad Kammin <bkammin@nypublicradio.org>
2019-12-09 12:26:28 -0500

    DSODA-293 add serializer tests

--- app/serializers/playlist-daily.js
+++ app/serializers/playlist-daily.js
@@ -4,25 +4,36 @@ import { camelizeObject } from 'nypr-publisher-lib/helpers/camelize-object';
 import moment from "moment";
 
 export default DS.JSONAPISerializer.extend({
-  normalizeFindRecordResponse(store, modelClass, payload, id/*, requestType*/) {
+  _removeUnairedEvents(payload) {
     payload.playlistDaily.events = payload.playlistDaily.events.filter(function(event) {
       return moment(get(event, 'start_timestamp')).format("X") <= moment().format("X");
     });
+  },
 
+  _sortEventsDescending(payload) {
+    payload.playlistDaily.events.sort(function(a, b) {
+      return (moment(get(a, 'start_timestamp'), 'YYYY-MM-DDTHH:mm:ss') > moment(get(b, 'start_timestamp'), 'YYYY-MM-DDTHH:mm:ss') ? -1 : 1);
+    });
+  },
+
+  _mergeAndSortPlaylists(payload) {
     payload.playlistDaily.events.forEach(function(event) {
       if (event.playlists) {
         var playlist = [];
 
+        // merge all playlists into one
         for (var i = 0, l = event.playlists.length; i < l; i++) {
           playlist = playlist.concat(event.playlists[i].played);
         }
 
+        // fix data fields
         playlist.forEach(function(track) {
           track.catalogEntry = track.info;
           track.startTime = track.time;
           delete track.info;
         });
 
+        // sort playlist in descending order
         playlist.sort(function(a, b) {
           return (moment(get(a, 'start_time'), 'hh:mm a') > moment(get(b, 'start_time'), 'hh:mm a') ? -1 : 1);
         });
@@ -31,10 +42,12 @@ export default DS.JSONAPISerializer.extend({
         delete event.playlists;
       }
     });
+  },
 
-    payload.playlistDaily.events.sort(function(a, b) {
-      return (moment(get(a, 'start_timestamp'), 'YYYY-MM-DDTHH:mm:ss') > moment(get(b, 'start_timestamp'), 'YYYY-MM-DDTHH:mm:ss') ? -1 : 1);
-    });
+  normalizeFindRecordResponse(store, modelClass, payload, id/*, requestType*/) {
+    this._removeUnairedEvents(payload);
+    this._mergeAndSortPlaylists(payload);
+    this._sortEventsDescending(payload);
 
     return {
       data: {
