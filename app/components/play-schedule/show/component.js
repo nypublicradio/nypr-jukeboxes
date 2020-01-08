8358dd7f9bde5d4f8b6bd98a2016fe5cdc54982e
Brad Kammin <bkammin@nypublicradio.org>
2020-01-08 10:25:45 -0500

    DSODA-336 in playlist history, display stream name if not current show title is available

--- app/components/play-schedule/show/component.js
+++ app/components/play-schedule/show/component.js
@@ -1,9 +1,11 @@
 //adapted from new-sounds-web-client
 import Component from '@ember/component';
+import { inject as service} from '@ember/service';
 
 export default Component.extend({
   classNames: ['play-schedule-show'],
   isPlaylistVisible: false,
+  nowPlaying: service(),
 
   actions: {
     togglePlaylistVisible() {
