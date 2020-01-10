5441a36cf1ba9251eca98e7ece462749ecd33108
Jeff Keen <jeff@keen.me>
2020-01-09 18:50:37 -0600

    Hide times in fastboot environment to combat jumpiness

--- app/components/play-schedule/show/component.js
+++ app/components/play-schedule/show/component.js
@@ -1,11 +1,14 @@
 //adapted from new-sounds-web-client
 import Component from '@ember/component';
-import { inject as service} from '@ember/service';
+import { inject as service } from '@ember/service';
+import { reads } from '@ember/object/computed';
 
 export default Component.extend({
   classNames: ['play-schedule-show'],
   isPlaylistVisible: false,
   nowPlaying: service(),
+  fastboot: service(),
+  isFastBoot: reads('fastboot.isFastBoot'),
 
   actions: {
     togglePlaylistVisible() {
--- app/components/track-info/component.js
+++ app/components/track-info/component.js
@@ -1,5 +1,9 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { reads } from '@ember/object/computed';
 
 export default Component.extend({
+	fastboot: service(),
+	isFastBoot: reads('fastboot.isFastBoot'),
 	classNames: ['song-metadata']
 });
