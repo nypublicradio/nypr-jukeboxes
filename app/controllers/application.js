8133d333e1d967beea35c9c582765d718f15b85c
Brad Kammin <bkammin@nypublicradio.org>
2020-01-24 14:36:16 -0500

    DSODA-163 add click tracking to `Playlist` tab clicks

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -13,7 +13,8 @@ export default Controller.extend({
   nowPlaying     : service(),
   fastboot       : service(),
   isFastBoot     : reads('fastboot.isFastBoot'),
-  links: [ { 'href': null, 'nav-slug': 'listen', 'route': 'listen', 'title': 'Listen'}, { 'href': null, 'nav-slug': 'playlist-history', 'route': 'playlist-history-today', 'title': 'Playlist'} ],
+  links: [ { 'href': null, 'nav-slug': 'listen', 'route': 'listen', 'title': 'Listen'},
+           { 'href': null, 'nav-slug': 'playlist-history', 'route': 'playlist-history-today', 'title': 'Playlist', 'clickTracking': true, 'dataAction': 'View Playlist'} ],
 
   showPlayer: true,
   showOnboardMessage: computed('closed', function() {
