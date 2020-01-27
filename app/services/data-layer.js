254694346291ea4ea9a6af3266529104d1278448
Brad Kammin <bkammin@nypublicradio.org>
2020-01-27 12:19:02 -0500

    DSODA-163 lint cleanup

--- app/services/data-layer.js
+++ app/services/data-layer.js
@@ -1,7 +1,6 @@
 import DataLayerService from 'nypr-metrics/services/data-layer';
 import { get } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { inject as controller } from '@ember/controller';
 import { getOwner } from '@ember/application';
 
 export default DataLayerService.extend({
@@ -10,21 +9,21 @@ export default DataLayerService.extend({
   _audioEventForType(soundObject) {
     let event = this._super(...arguments);
 
-    let { contentModelType:type, contentModel:model } = get(soundObject, 'metadata');
+    let { contentModelType:type } = get(soundObject, 'metadata');
 
     if (type != 'stream') {
-    	return event;
+      return event;
     }
 
     let owner = getOwner(this);
     let listenController = owner.lookup('controller:listen');
     return Object.assign(event, {
-    	'whatsOn': get(this.nowPlaying, 'track') !== null ? 1 : 0,
-    	'playlistPreview': get(listenController, 'recentlyPlayed.length') > 0 ? 1 : 0,
-    	'composer': get(this.nowPlaying, 'composerName'),
-    	'hostName': get(this.nowPlaying, 'showHost'),
-    	'showName': get(this.nowPlaying, 'showTitle'),
-    	'track': get(this.nowPlaying, 'trackTitle'),
+      'whatsOn': get(this.nowPlaying, 'track') !== null ? 1 : 0,
+      'playlistPreview': get(listenController, 'recentlyPlayed.length') > 0 ? 1 : 0,
+      'composer': get(this.nowPlaying, 'composerName'),
+      'hostName': get(this.nowPlaying, 'showHost'),
+      'showName': get(this.nowPlaying, 'showTitle'),
+      'track': get(this.nowPlaying, 'trackTitle'),
     });
   }
 });
