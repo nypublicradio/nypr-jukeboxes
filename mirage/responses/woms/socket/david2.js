ae969628dd1c702f1a5502c9df9b85eb890b60a5
Jeff Keen <jeff@keen.me>
2020-02-03 12:21:19 -0600

    More updates to woms serializer for upcoming attribute changes

--- mirage/responses/woms/socket/david2.js
+++ mirage/responses/woms/socket/david2.js
@@ -2,83 +2,65 @@
 
 export default function() {
   return {
-    "Item": {
-      "metadata": {
-        "album": "Stokowski | Transcriptions",
-        "epoch_start_time": 1579038687,
-        "mm_composer1": "Richard Wagner",
-        "mm_conductor": "Jose Serebrier, conductor",
-        "length": "485935",
-        "title": "Das Rheingold: 'Entry of Gods into Valhalla'",
-        "mm_ensemble1": "Bournemouth Symphony Orchestra",
-        "iso_start_time": "2020-01-13T18:45:13+00:00",
-        "start_time": "2020-01-13 13:45:13.143",
-        "david_guid": "{8BA3731B-7C78-4692-9E25-6E80153E115F}",
-        "mm_reclabel": "Naxos",
-        "catno": "8578305",
-        "mm_uid": "146376",
-        "playlist_hist_preview": [
-          {
-            "album": "Dvorak: Serenades",
-            "epoch_start_time": 1578940093,
-            "mm_composer1": "Antonin Dvorak",
-            "mm_conductor": "Myung-Whun Chung, conductor",
-            "length": "1375239",
-            "title": "Serenade in D Minor for Wind Ensemble, Op. 44 (B77)",
-            "mm_ensemble1": "Members of the Vienna Philharmonic",
-            "iso_start_time": "2020-01-13T18:28:13+00:00",
-            "start_time": "2020-01-13 13:28:13.143",
-            "david_guid": "{E79379D2-D926-492B-947B-0DE705EBCE1A}",
-            "mm_reclabel": "Deutsche Grammophon",
-            "catno": "471 613-2",
-            "mm_uid": "125296",
-          },
-          {
-            "mm_ensemble1": "Cleveland Orchestra",
-            "iso_start_time": "2020-01-13T18:21:09+00:00",
-            "start_time": "2020-01-13 13:21:09.563",
-            "david_guid": "{A6E11D35-5488-43DA-A040-69263B55AB83}",
-            "epoch_start_time": 1578939669,
-            "mm_reclabel": "Deutsche Grammophon",
-            "mm_composer1": "Maurice Ravel",
-            "mm_conductor": "Pierre Boulez, conductor",
-            "catno": "457693",
-            "length": "396266",
-            "title": "Pavane for a Dead Princess",
-            "mm_uid": "110109"
-          },
-          {
-            "album": "Haydn Piano Sonatas",
-            "epoch_start_time": 1578938385,
-            "mm_composer1": "Franz Joseph Haydn",
-            "mm_conductor": "Scott Yoo, conductor",
-            "length": "1179708",
-            "title": "Piano Concerto in D Hob. 18",
-            "mm_ensemble1": "Odense Symphony Orchestra",
-            "iso_start_time": "2020-01-13T17:59:45+00:00",
-            "start_time": "2020-01-13 12:59:45.999",
-            "david_guid": "{5D2B55F5-851D-4A2E-A18D-1062DEA8D64A}",
-            "mm_reclabel": "Bridge",
-            "catno": "9438A/B",
-            "mm_soloist1": "Anne-Marie McDermott, piano",
-            "mm_uid": "142369"
-          }
-        ]
-      }
-    },
-    "ResponseMetadata": {
-      "RequestId": "8ONRHLE74I7OHVIJ59FS74GEQVVV4KQNSO5AEMVJF66Q9ASUAAJG",
-      "HTTPStatusCode": 200,
-      "HTTPHeaders": {
-        "server": "Server",
-        "date": "Mon, 13 Jan 2020 18:48:46 GMT",
-        "content-type": "application/x-amz-json-1.0",
-        "content-length": "2230",
-        "connection": "keep-alive",
-        "x-amzn-requestid": "8ONRHLE74I7OHVIJ59FS74GEQVVV4KQNSO5AEMVJF66Q9ASUAAJG",
-        "x-amz-crc32": "673520291"
+    "album": "Stokowski | Transcriptions",
+    "epoch_start_time": 1579038687,
+    "mm_composer1": "Richard Wagner",
+    "mm_conductor": "Jose Serebrier, conductor",
+    "length": "485935",
+    "title": "Das Rheingold: 'Entry of Gods into Valhalla'",
+    "mm_ensemble1": "Bournemouth Symphony Orchestra",
+    "iso_start_time": "2020-01-13T18:45:13+00:00",
+    "start_time": "2020-01-13 13:45:13.143",
+    "david_guid": "{8BA3731B-7C78-4692-9E25-6E80153E115F}",
+    "mm_reclabel": "Naxos",
+    "catno": "8578305",
+    "mm_uid": "146376",
+    "playlist_hist_preview": [
+      {
+        "album": "Dvorak: Serenades",
+        "epoch_start_time": 1578940093,
+        "mm_composer1": "Antonin Dvorak",
+        "mm_conductor": "Myung-Whun Chung, conductor",
+        "length": "1375239",
+        "title": "Serenade in D Minor for Wind Ensemble, Op. 44 (B77)",
+        "mm_ensemble1": "Members of the Vienna Philharmonic",
+        "iso_start_time": "2020-01-13T18:28:13+00:00",
+        "start_time": "2020-01-13 13:28:13.143",
+        "david_guid": "{E79379D2-D926-492B-947B-0DE705EBCE1A}",
+        "mm_reclabel": "Deutsche Grammophon",
+        "catno": "471 613-2",
+        "mm_uid": "125296",
       },
-      "RetryAttempts": 0
-    }
+      {
+        "mm_ensemble1": "Cleveland Orchestra",
+        "iso_start_time": "2020-01-13T18:21:09+00:00",
+        "start_time": "2020-01-13 13:21:09.563",
+        "david_guid": "{A6E11D35-5488-43DA-A040-69263B55AB83}",
+        "epoch_start_time": 1578939669,
+        "mm_reclabel": "Deutsche Grammophon",
+        "mm_composer1": "Maurice Ravel",
+        "mm_conductor": "Pierre Boulez, conductor",
+        "catno": "457693",
+        "length": "396266",
+        "title": "Pavane for a Dead Princess",
+        "mm_uid": "110109"
+      },
+      {
+        "album": "Haydn Piano Sonatas",
+        "epoch_start_time": 1578938385,
+        "mm_composer1": "Franz Joseph Haydn",
+        "mm_conductor": "Scott Yoo, conductor",
+        "length": "1179708",
+        "title": "Piano Concerto in D Hob. 18",
+        "mm_ensemble1": "Odense Symphony Orchestra",
+        "iso_start_time": "2020-01-13T17:59:45+00:00",
+        "start_time": "2020-01-13 12:59:45.999",
+        "david_guid": "{5D2B55F5-851D-4A2E-A18D-1062DEA8D64A}",
+        "mm_reclabel": "Bridge",
+        "catno": "9438A/B",
+        "mm_soloist1": "Anne-Marie McDermott, piano",
+        "mm_uid": "142369"
+      }
+    ]
   }
 }
