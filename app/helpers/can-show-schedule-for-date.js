e558a67885711726130e512b161761c720a6bb3e
Seori <seori@gmail.com>
2020-02-21 17:17:08 -0500

    Start of post today time

--- app/helpers/can-show-schedule-for-date.js
+++ app/helpers/can-show-schedule-for-date.js
@@ -5,11 +5,17 @@ export function helperFunction(params) {
   console.log('today date', params[0]) 
   console.log('what date', params[1]) //1
   let momentDate = moment(params[0],'YYYY-MM-DDTHH:mm:ss').add(params[7], 'days').startOf('day');
-  return !isAfterToday(momentDate);
+
+  console.log('isaftertoday', isAfterToday(momentDate))
+  return true;
+  //return !isAfterToday(momentDate);
+  //return isAfterToday(momentDate); //doesnt show the next day's link
 }
 
 function isAfterToday(momentDate) {
   let today = moment().startOf('day');
+  console.log(today)
+  
   return momentDate.isAfter(today, 'd');
 }
 
