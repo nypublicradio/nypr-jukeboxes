0d17e3e90fbfe9b48f9b75402975f7c8d80abe67
Seori <seori@gmail.com>
2020-03-12 22:38:24 -0400

    Test coverage up

--- tests/integration/helpers/formatted-day-test.js
+++ tests/integration/helpers/formatted-day-test.js
@@ -7,17 +7,21 @@ import moment from 'moment';
 module('Integration | Helper | formatted-day', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders a date with year, month, date, and time if today', async function(assert) {
     let today = moment();
     this.set('inputValue', today.format('YYYY-MM-DDTHH:mm:ss'));
     await render(hbs`{{formatted-day inputValue}}`);
     assert.equal(this.element.textContent.trim(), 'Today');
+  })
 
+  test('it renders a date with year, month, date, and time if yesterday', async function(assert) {
     let yesterday = moment().add(-1, 'days');
     this.set('inputValue', yesterday.format('YYYY-MM-DDTHH:mm:ss'));
     await render(hbs`{{formatted-day inputValue}}`);
     assert.equal(this.element.textContent.trim(), 'Yesterday');
-
+  })
+  
+  test('it renders a date with year, month, date, and time if day before yesterday', async function(assert) {
     let beforeYesterday = moment().add(-2, 'days');
     this.set('inputValue', beforeYesterday.format('YYYY-MM-DDTHH:mm:ss'));
     await render(hbs`{{formatted-day inputValue}}`);
